0f815fc264bc9eb4da6d8a62e11df7d60589327a
Leticia Cristina <133662502+Leticianha@users.noreply.github.com>
2023-10-16 11:50:28 -0300

    Add files via upload

---
+++ 03/banco.js
@@ -0,0 +1,7 @@
+let estadoFuncao = require("./estado")
+let idadeFuncao = require("./idade")
+let nomeFuncao = require("./nome")
+
+console.log(estadoFuncao("sp"))
+console.log(idadeFuncao(18))
+console.log(nomeFuncao)
